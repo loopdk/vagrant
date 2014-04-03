9724a650d6a5b24cbf125bd841618a6bf93bea10
Thomas Johansen <thsj@aarhus.dk>
2014-04-03 08:25:52 +0200

    Added timezone and cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,51 +1,82 @@
 #!/usr/bin/env bash
 
-# Drush
-echo "Installing drush"
-apt-get install -y php-pear > /dev/null 2>&1
+# APT
+echo "Updating system (apt)..."
+apt-get update
+apt-get upgrade
 
+# Set timezone.
+echo "Setting timezone..."
+echo "Europe/Copenhagen" > /etc/timezone
+/usr/sbin/dpkg-reconfigure --frontend noninteractive tzdata #> /dev/null 2>&1
+
+# Set locale
+echo "Setting up locale..."
+echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+echo en_DK.UTF-8 UTF-8 >> /etc/locale.gen
+echo da_DK.UTF-8 UTF-8 >> /etc/locale.gen
+/usr/sbin/locale-gen #> /dev/null 2>&1
+export LANGUAGE=en_DK.UTF-8 > /dev/null 2>&1
+export LC_ALL=en_DK.UTF-8 > /dev/null 2>&1
+/usr/sbin/dpkg-reconfigure --frontend noninteractive locales
+
+# Drush
+echo "Installing drush..."
+apt-get install -y php-pear > /dev/null 2>&1
 pear channel-discover pear.drush.org > /dev/null 2>&1
 pear install drush/drush > /dev/null 2>&1
-
 drush version > /dev/null 2>&1
 
 # Apache config
-echo "Configuing apache"
-apt-get -y install php5-mysql libapache2-mod-php5 php5-gd php-db apache2 lsyncd php5-curl > /dev/null 2>&1
-
+echo "Configuring Apache..."
+apt-get -y install php5-mysql libapache2-mod-php5 php5-gd php-db apache2 php5-curl > /dev/null 2>&1
 rm -rf /var/www
 ln -s /vagrant/htdocs /var/www
-
 sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
 sed -i '/export APACHE_RUN_USER=www-data/c export APACHE_RUN_USER=vagrant' /etc/apache2/envvars
 sed -i '/export APACHE_RUN_GROUP=www-data/c export APACHE_RUN_GROUP=vagrant' /etc/apache2/envvars
 sed -i '/memory_limit = 128M/c memory_limit = 512M' /etc/php5/apache2/php.ini
 chown vagrant:vagrant /var/lock/apache2
-
 a2enmod rewrite > /dev/null 2>&1
-
 a2enmod php5 > /dev/null 2>&1
+a2enmod expires > /dev/null 2>&1
+
+# Configura PHP
+echo "Configuring up PHP..."
+sed -i '/memory_limit = 128M/c memory_limit = 512M' /etc/php5/apache2/php.ini
+sed -i ';date.timezone =/c date.timezone = Europe/Copenhagen' /etc/php5/apache2/php.ini
+sed -i ';date.timezone =/c date.timezone = Europe/Copenhagen' /etc/php5/cli/php.ini
+sed -i '/upload_max_filesize = 2M/c upload_max_filesize = 16M' /etc/php5/apache2/php.ini
+sed -i '/post_max_size = 8M/c post_max_size = 20M' /etc/php5/apache2/php.ini
+sed -i '/;realpath_cache_size = 16k/c realpath_cache_size = 256k' /etc/php5/apache2/php.ini
+
+pecl install uploadprogress > /dev/null 2>&1
+echo "extension=uploadprogress.so" > /etc/php5/conf.d/uploadprogress.ini
 
 # Mysql
-echo "Configuring mysql"
-
+echo "Installing MySQL..."
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password vagrant' > /dev/null 2>&1
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password vagrant' > /dev/null 2>&1
-
 apt-get install -y mysql-server > /dev/null 2>&1
 
-echo "Starting mysql"
+# Configure MySQL
+echo "Configuring MySQL..."
+cat > /etc/mysql/conf.d/innodb.cnf <<DELIM
+[mysqld]
+innodb_buffer_pool_size=256M
+innodb_flush_method=O_DIRECT
+innodb_additional_mem_pool_size=10M
+innodb_flush_log_at_trx_commit=0
+innodb_thread_concurrency=6
+DELIM
 
 # Memcache
-echo "Install memcache"
-
+echo "Installing MemCached"
 apt-get install -y memcached php5-memcached > /dev/null 2>&1
 
 # APC
 echo "Configuring APC"
-
 apt-get install -y php-apc > /dev/null 2>&1
-
 cat > /etc/php5/conf.d/apc.ini <<DELIM
 apc.enabled=1
 apc.shm_segments=1
@@ -61,44 +92,37 @@ DELIM
 
 # Solr
 echo "Configuring Solr"
-
 apt-get install -y java7-jdk tomcat7 > /dev/null 2>&1
-
 cd ~
-
 wget http://ftp.download-by.net/apache/lucene/solr/4.7.0/solr-4.7.0.tgz -O solr.tgz > /dev/null 2>&1
-
 tar xzf solr.tgz
 rm solr.tgz
-
 cp solr-4.7.0/example/lib/ext/* /usr/share/tomcat7/lib/
-
 cp solr-4.7.0/dist/solr-4.7.0.war /var/lib/tomcat7/webapps/solr.war
-
 cp -R solr-4.7.0/example/solr /var/lib/tomcat7
-
 rm -rf solr-4.7.0
-
 mv /var/lib/tomcat7/solr/collection1 /var/lib/tomcat7/solr/loop_stg
-
 echo "name=loop_stg" > /var/lib/tomcat7/solr/loop_stg/core.properties
 
 drush dl search_api_solr
 cp search_api_solr/solr-conf/4.x/* /var/lib/tomcat7/solr/loop_stg/conf/
-
 rm -rf search_api_solr
-
 sed -i '/\<Connector port="8080" protocol="HTTP\/1.1"/c \<Connector port="8983" protocol="HTTP\/1.1"' /var/lib/tomcat7/conf/server.xml
-
 chown -R tomcat7:tomcat7 /var/lib/tomcat7/solr
 
-service tomcat7 restart
+
+# Configure Varnish
+echo "Installing Varnish..."
+wget http://repo.varnish-cache.org/debian/GPG-key.txt > /dev/null 2>&1
+apt-key add GPG-key.txt > /dev/null 2>&1
+rm GPG-key.txt
+echo "deb http://repo.varnish-cache.org/debian/ $(lsb_release -s -c) varnish-3.0" > /etc/apt/sources.list.d/varnish.list
+apt-get update > /dev/null 2>&1
+apt-get install varnish -y > /dev/null 2>&1
 
 # Varnish
-echo "Configuring varnish"
-
+echo "Configuring varnish..."
 apt-get install -y varnish > /dev/null 2>&1
-
 cat > /etc/default/varnish <<DELIM
 START=yes
 NFILES=131072
@@ -117,10 +141,12 @@ backend default {
 }
 DELIM
 
-echo "Starting apache"
+# Restart services
+echo "Restarting services..."
 service apache2 restart > /dev/null 2>&1
+service mysql restart > /dev/null 2>&1
+service memcached restart > /dev/null 2>&1
+service varnishd restart > /dev/null 2>&1
+service tomcat7 restart > /dev/null 2>&1
 
-echo "Starting varnish"
-service varnish restart > /dev/null 2>&1
-
-echo "Done"
+echo "Provisioning completed..."
